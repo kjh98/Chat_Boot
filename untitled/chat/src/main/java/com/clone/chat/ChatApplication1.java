311179a8e8f8e9b2e95694a3ac63f8c76f959dd6
kjh98 <kjh92512@gmail.com>
2021-11-17 18:09:50 +0900

    채팅 컨트롤러 생성

--- untitled/chat/src/main/java/com/clone/chat/ChatApplication1.java
+++ untitled/chat/src/main/java/com/clone/chat/ChatApplication1.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatApplication {
+public class ChatApplication1 {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatApplication.class, args);
+        SpringApplication.run(ChatApplication1.class, args);
     }
 
 }
